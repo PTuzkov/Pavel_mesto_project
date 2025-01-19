494c1afd07bab9f11126e050aaa4b77b28d645a8
PTuzkov <flanber94@gmail.com>
2025-01-19 15:21:59 +0300

    Версия 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
 
 // @todo: DOM узлы
+// Контейнер для карточек
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, handleDeleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    // Значения вложенных элементов
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    cardImage.alt = cardData.name;
+    cardImage.src = cardData.link;
+    cardTitle.textContent = cardData.name;
+
+    // @todo: Функция удаления карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        handleDeleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
+// @todo: Функция добавления карточки на страницу
+function addCardToPage(cardData) {
+    const cardElement = createCard(cardData, (cardElement) => {
+        cardElement.remove(); // Удаляем карточку из DOM
+    });
+    placesList.append(cardElement); // Добавляем карточку в список
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(addCardToPage);
